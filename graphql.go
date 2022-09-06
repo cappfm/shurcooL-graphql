8a35cbba1e9973c738ec8a211de6aa87620a234f
Gawen Arab <gawen@cappuccino.fm>
2022-09-06 20:22:03 +0200

    errors: add path field

--- graphql.go
+++ graphql.go
@@ -103,6 +103,7 @@ func (c *Client) do(ctx context.Context, op operationType, v interface{}, variab
 // Specification: https://facebook.github.io/graphql/#sec-Errors.
 type Errors []struct {
 	Message   string
+	Path      []string
 	Locations []struct {
 		Line   int
 		Column int
